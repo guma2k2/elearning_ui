8534e879cc3a2b33613dff79fd7ab26cfa5422a1
thuan <thuanngo30072002@gmail.com>
2024-02-08 14:27:23 +0700

    course feature

---
+++ src/components/answer/index.tsx
@@ -0,0 +1,45 @@
+import { useRef, useState } from 'react';
+import './Answer.style.scss'
+import { FaTrash } from 'react-icons/fa';
+import ReactQuill from 'react-quill';
+
+const answerodules = {
+    toolbar: [
+        ['bold', 'italic'], ['code-block']
+    ],
+};
+
+const answerFormats = [
+    'bold', 'italic', 'code-block',
+];
+function Answer() {
+    const [toggle, setToggle] = useState<boolean>(false)
+    const [answerDesc, setAnswerDesc] = useState<string>("")
+    const radioRef = useRef<HTMLInputElement | null>(null)
+    const handleChecked = (e: any) => {
+        if (radioRef.current) {
+            radioRef.current.checked = true;
+        }
+    };
+
+    return (
+        <div className="answer-container">
+            <div className="radio">
+                <input ref={radioRef} checked type="radio" name="radio" className='input-radio' />
+                <span className='input-container'>
+                    {toggle == false && <p className='answer-text' onClick={() => setToggle(true)}>
+                        <span>Add an answer</span>
+                    </p>}
+                    {toggle == true && <div className="question-rte" ><ReactQuill modules={answerodules} formats={answerFormats} theme="snow" value={answerDesc} onChange={setAnswerDesc} /></div>}
+                    <input type="text" className='answer-explain' placeholder="Explain Why this is or isn't the best answer" />
+                </span>
+                <span onClick={handleChecked} className="checkmark"></span>
+            </div>
+            <div className="answer-icon">
+                <FaTrash className="icon-trash" />
+            </div>
+        </div>
+    )
+}
+
+export default Answer
--- src/components/curriculum/index.tsx
+++ src/components/curriculum/index.tsx
@@ -11,29 +11,41 @@ import ReactQuill from "react-quill"
 import 'react-quill/dist/quill.snow.css';
 import InputFile from "../inputFile"
 import { LiaTimesSolid } from "react-icons/lia"
+import Answer from "../answer"
 
 type ToggleType = {
-    type: "desc" | "resources" | "lecture" | "quiz" | "dropdown" | ""
+    type: "desc" | "resources" | "lecture" | "quiz" | "dropdown" | "" | "content" | "questions"
 }
 type FieldType = {
     title?: string;
     url?: string;
 };
-const modules = {
+const lectureModules = {
     toolbar: [
         ['bold', 'italic'],
         [{ 'list': 'ordered' }, { 'list': 'bullet' }],  // Customize the toolbar to include only bold and italic options
     ],
 };
 
-const formats = [
+const lectureFormats = [
     'bold', 'italic', 'list', 'bullet',
 ];
-function Curriculum(probs: CurriculumType) {
 
+const questionModules = {
+    toolbar: [
+        ['bold', 'italic'],
+        ['code-block'], ['image']
+    ],
+};
+
+const questionFormats = [
+    'bold', 'italic', 'code-block', "image"
+];
+function Curriculum(probs: CurriculumType) {
     const curriculumHeaderRef = useRef<HTMLDivElement | null>(null);
     const [toggle, setToggle] = useState<ToggleType>({ type: "" });
     const [lectureDesc, setLectureDesc] = useState<string>("");
+    const [questionDesc, setQuestionDesc] = useState<string>("");
     const onFinish = (values: FieldType) => {
         console.log(values);
     }
@@ -109,7 +121,8 @@ function Curriculum(probs: CurriculumType) {
                         </span>
                     </div>
                     <div className="curriculum-right">
-                        {toggle.type !== "resources" ? <Button className='btn-curriculum' icon={<AiOutlinePlus />}>{probs.type == "lecture" ? "Content" : "Questions"}</Button> : ""}
+                        {toggle.type !== "resources" && toggle.type !== "content" && probs.type == "lecture" && <Button onClick={() => setToggle({ type: "content" })} className='btn-curriculum' icon={<AiOutlinePlus />}>Content</Button>}
+                        {toggle.type !== "resources" && toggle.type !== "questions" && probs.type == "quiz" && <Button onClick={() => setToggle({ type: "questions" })} className='btn-curriculum' icon={<AiOutlinePlus />}>Questions</Button>}
                         {probs.type == "lecture" && toggle.type == "" && <IoIosArrowDown onClick={() => setToggle({ type: "dropdown" })} />}
                         {probs.type == "lecture" && toggle.type == "dropdown" && <IoIosArrowUp onClick={() => setToggle({ type: "" })} />}
                     </div>
@@ -122,7 +135,7 @@ function Curriculum(probs: CurriculumType) {
                 {toggle.type == "desc" && <div className="curriculum-dropdown">
                     <div className="dropdown-top">
                         <span>Lecture Description</span>
-                        <div className="lecture-rte"><ReactQuill modules={modules} formats={formats} theme="snow" value={lectureDesc} onChange={setLectureDesc} placeholder="Add a description. Include what students will be able to do after completing the lecture." /></div>
+                        <div className="lecture-rte"><ReactQuill modules={lectureModules} formats={lectureFormats} theme="snow" value={lectureDesc} onChange={setLectureDesc} placeholder="Add a description. Include what students will be able to do after completing the lecture." /></div>
                         <div className="lecture-form-action">
                             <div style={{ cursor: "pointer" }} className="cancel" onClick={() => setToggle({ type: "dropdown" })}>Cancel</div>
                             <Button type="primary">Save</Button>
@@ -140,6 +153,40 @@ function Curriculum(probs: CurriculumType) {
                         <Tabs defaultActiveKey="1" items={items} />
                     </div>
                 </div>
+                }
+                {
+                    toggle.type == "content" &&
+                    <div className="curriculum-dropdown">
+                        <div className="dropdown-bottom">
+                            <div className="tab-title">
+                                <span>Add Video</span>
+                                <span className="tab-title-icon" onClick={() => setToggle({ type: "dropdown" })}><LiaTimesSolid /></span>
+                            </div>
+                            <InputFile title="Select Video" />
+                        </div>
+                    </div>
+                }
+                {toggle.type == "questions" &&
+                    <div className="curriculum-dropdown">
+                        <div className="dropdown-question-bottom">
+                            <div className="tab-title">
+                                <span>Add Multiple Choice</span>
+                                <span className="tab-title-icon" onClick={() => setToggle({ type: "" })}><LiaTimesSolid /></span>
+                            </div>
+                        </div>
+                        <div className="curriculum-question">
+                            <span>Question</span>
+                            <div className="question-rte"><ReactQuill modules={questionModules} formats={questionFormats} theme="snow" value={questionDesc} onChange={setQuestionDesc} /></div>
+                        </div>
+                        <div className="curriculum-answers">
+                            <span>Answer</span>
+                            <div className="curriculum-answers-container">
+                                <Answer />
+                                <Answer />
+                                <Answer />
+                            </div>
+                        </div>
+                    </div>
                 }
             </div>
         </>
